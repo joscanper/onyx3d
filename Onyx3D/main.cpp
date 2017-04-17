d7ad6a6d0c096515905ebfbd069344d854da4034
Josue Candela Perdomo <josuecp88@gmail.com>
2017-04-17 20:50:07 +0100

    Initial Commit

---
+++ Onyx3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Onyx3D
+//
+//  Created by Josue Candela Perdomo on 17/04/2017.
+//  Copyright (c) 2017 Josue Candela. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
